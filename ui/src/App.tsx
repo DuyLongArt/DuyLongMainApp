45ab90063fa4c1b244dcfec36e8f6e701151702a
DuyLong <duylongmind432001@gmail.com>
2025-05-27 00:09:48 +0700

    Create default template with Vite, React, Node

---
+++ ui/src/App.tsx
@@ -0,0 +1,19 @@
+import duylongAppIcon from './assets/AppMainIcon.png'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>
+        DuyLong App
+      </h1>
+      <div>
+        <a href="http://0.0.0.0:22222/" target="_blank">
+          <img src={duylongAppIcon} className="logo" alt="Vite logo" />
+        </a>
+      </div>
+    </>
+  )
+}
+
+export default App
